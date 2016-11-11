44f2b778215949d0370e39790a04738760630dd3
Karen Casey-Gover <karen.casey@hotmail.com>
2016-11-11 20:52:05 +0000

    added validations to model Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  belongs_to :product
+
+  validates :product_id, :user_id, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true, length: { maximum: 250}
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
